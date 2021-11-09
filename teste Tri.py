1a3f9836d749b3f9bd4e260978dcd92bf675ad2f
Silver-A-DRAKE <81689421+Silver-A-DRAKE@users.noreply.github.com>
2021-11-09 12:55:23 +0100

    Tri algo

---
+++ teste Tri.py
@@ -0,0 +1,13 @@
+#Quick sort
+def qsort(u):
+    if u == []:
+        pivot. g. d = u[0],[],[]
+        for i in u[1:]: g.append(x)
+        if x<pivot
+        else d.append(x)
+        return qsort(g)+[u[0]]+qsort(d)
+
+liste=[21,32,37,18,99,30,74,23,52,59]
+liste tri=qsort(liste)
+print('liste initiale:', liste)
+print('liste triée:', liste_tri)
